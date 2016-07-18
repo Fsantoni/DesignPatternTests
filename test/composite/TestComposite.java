5388bbd585b0e665c322a3e612b05c42cec824e5
Francesco <Francesco@Santoni>
2016-07-18 12:18:40 +0200

    prima aggiunta Junit test

---
+++ test/composite/TestComposite.java
@@ -0,0 +1,33 @@
+package composite;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestComposite {
+
+	@Test
+	public final void testComponent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetValue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRemove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetChild() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
