c64af835c6005895f1d27f448245fb3c502894aa
Francesco <Francesco@Santoni.lan>
2016-07-28 19:12:54 +0200

    aggiunto errore, da controllare e modificare disegni object adapter

--- test/objectAdapter/TestObjectAdapter.java
+++ test/objectAdapter/TestObjectAdapter.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestObjectAdapter {
-	
+
 
 	@Test
 	public final void testAdapterGetInt() {
@@ -14,7 +14,7 @@ public class TestObjectAdapter {
 
 		assertEquals(0,result);
 	}
-	
+
 	@Test
 	public final void testAdapterSetIntGetInt() {
 
@@ -24,18 +24,27 @@ public class TestObjectAdapter {
 
 		assertEquals(0,result);
 	}
-	
-	
+
+
 
 	@Test
 	public final void testAdapterGetInt2() {
-		
+
 		Adaptee a =new AdapteeOpposite();
 		ObjectAdapter adapt = new ObjectAdapter(a);
-		
-			int result=adapt.getIntResult();
+
+		int result=adapt.getIntResult();
 
 		assertEquals(0,result);
+
+		Adaptee b =new AdapteeOpposite();
+		ObjectAdapter adapt1 = new ObjectAdapter(b);
+
+		b.setBooleanResult(true);
+		int result1 =adapt1.getIntResult();
+
+
+		assertEquals(1,result1);
 	}
 
 
